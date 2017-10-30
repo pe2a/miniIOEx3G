ebfb765c8684000fdd2ddbf7a7683738082c655b
Polly Electronics <yanikgo@itu.edu.tr>
2017-10-30 16:31:55 +0300

    first commit

---
+++ miniIOEx-3G-test.py
@@ -0,0 +1,21 @@
+import serial
+import os,time
+
+
+
+port = serial.Serial('/dev/ttyUSB2',baudrate = 115200,timeout=1)
+
+port.write('ATI'+'\r\n')
+time.sleep(1)
+port.write('AT+CPIN?'+'\r\n')
+time.sleep(1)
+port.write('AT+CREG?'+'\r\n')
+time.sleep(1)
+port.write('AT+CSQ'+'\r\n')
+time.sleep(1)
+print "\n"
+print "operator: "
+port.write('AT+COPS?'+'\r\n')
+time.sleep(1)
+
+
