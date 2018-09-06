1308b60f55aa22e3e3bc1d0e12a3a3a164325ba1
Polly Electronics <yanikgo@itu.edu.tr>
2018-09-06 11:57:44 +0300

    Update output.py

--- output.py
+++ output.py
@@ -4,10 +4,10 @@ import RPi.GPIO as GPIO
 import time
 import random 
 
-DO_Relay1 = 21 #relay1 1A,24VDC
-DO_Relay2 = 20 #relay2 1A,24VDC
-DO_Mosfet1 = 19 #mosfet 2A,5VDC
-DO_Mosfet2 = 16 #mosfet, 2A,5VDC
+DO_Relay1 = 19 #relay1 1A,24VDC
+DO_Relay2 = 16 #relay2 1A,24VDC
+DO_TR1 = 21 #TRANSISTOR 2A,5VDC
+DO_TR2 = 20 #TRANSISTOR, 2A,5VDC
 DO_RunLed = 26 #RunLED on PCB 
 
 #init function
@@ -15,14 +15,13 @@ GPIO.setmode(GPIO.BCM) #bcm library
 
 GPIO.setup(DO_Relay1,GPIO.OUT)
 GPIO.setup(DO_Relay2,GPIO.OUT)
-GPIO.setup(DO_Mosfet1,GPIO.OUT) #LOW'da ON, HIGH'da OFF
-GPIO.setup(DO_Mosfet2,GPIO.OUT) #LOW'da ON, HIGH'da OFF
+GPIO.setup(DO_TR1,GPIO.OUT) #LOW ON, HIGH OFF
+GPIO.setup(DO_TR2,GPIO.OUT) #LOW ON, HIGH OFF
 GPIO.setup(DO_RunLed,GPIO.OUT)
 
 GPIO.setwarnings(False)
 
 while (1):
-	
 	
 	
 	#led 
@@ -44,15 +43,15 @@ while (1):
 	time.sleep(0.2) #200ms
 	
 	#mosfet1 
-	GPIO.output(DO_Mosfet1,GPIO.LOW) #ON
+	GPIO.output(DO_TR1,GPIO.LOW) #ON
 	time.sleep(0.2) #200ms
-	GPIO.output(DO_Mosfet1,GPIO.HIGH) #OFF
+	GPIO.output(DO_TR1,GPIO.HIGH) #OFF
 	time.sleep(0.2) #200ms
 	
 	#mosfet2 
-	GPIO.output(DO_Mosfet2,GPIO.LOW) 
+	GPIO.output(DO_TR2,GPIO.LOW) 
 	time.sleep(0.2) #200ms
-	GPIO.output(DO_Mosfet2,GPIO.HIGH) 
+	GPIO.output(DO_TR2,GPIO.HIGH) 
 	time.sleep(0.2) #200ms
 	
 
